7d4d035b8cdf8f81088ef25aab9dfe8a85039929
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-20 15:51:37 +0530

    Marketplace contract added with deployment and tests

---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Marketplace {
+    
+    string public name;
+
+    constructor () {
+        name = 'My Marketplace';
+    }    
+
+}
