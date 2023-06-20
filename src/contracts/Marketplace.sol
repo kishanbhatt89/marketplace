f23dd993384a6007fb77deb39ad87ae95df1999d
Kishan Bhatt <iamkishanbhatt@gmail.com>
2023-06-20 16:17:18 +0530

    Create product function added with tests

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -4,9 +4,42 @@ pragma solidity ^0.8.0;
 contract Marketplace {
     
     string public name;
+    uint public productCount = 0;
+
+    mapping (uint => Product) public products;
+
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
     constructor () {
         name = 'My Marketplace';
     }    
+
+    function createProduct (string memory _name, uint _price) public {
+
+        require(bytes(_name).length > 0);
+
+        require(_price > 0);
+
+        productCount++;
+
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
+
+    }
 
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,12 @@
+const { assert } = require("chai")
+
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
   let marketplace
 
   before(async () => {
@@ -22,4 +28,31 @@ contract('Marketplace', (accounts) => {
     })
 
   })
+
+  describe('products', () => {
+    let result, productCount;
+
+    before(async () => {
+        result = await marketplace.createProduct('P1', web3.utils.toWei('1','Ether'), {from: seller})
+        productCount = await marketplace.productCount()
+    })
+
+    it('create products', async () => {
+        assert.equal(productCount, 1)
+
+        const event = result.logs[0].args
+
+        assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+        assert.equal(event.name, 'P1', 'name is correct')
+        assert.equal(event.price, '1000000000000000000', 'price is correct')
+        assert.equal(event.owner, seller, 'owner is correct')
+        assert.equal(event.purchased, false, 'purchased is correct')
+
+        await await marketplace.createProduct('', web3.utils.toWei('1','Ether'), {from:seller}).should.be.rejected;
+
+        await await marketplace.createProduct('P1', 0, {from:seller}).should.be.rejected;
+    })
+
+  })
+
 })
